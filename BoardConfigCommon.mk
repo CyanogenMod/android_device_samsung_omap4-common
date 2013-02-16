93070ef772ee27ef63890b001dda1fde8ea11696
Andrew Jiang <sam.andrew.jiang@gmail.com>
2013-02-16 06:05:54 -0500

    omap4-common: Consolidate CPU configs & opt-in to bionic optimizations
    Change-Id: I52c14538432968971f57f724167dda2d49949564

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -21,6 +21,17 @@ BOARD_VENDOR := samsung
 PRODUCT_VENDOR_KERNEL_HEADERS := $(COMMON_PATH)/kernel-headers
 TARGET_SPECIFIC_HEADER_PATH := $(COMMON_PATH)/include
 
+# CPU
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_GLOBAL_CFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
+
 # HWComposer
 BOARD_USES_HWCOMPOSER := true
 BOARD_USE_SYSFS_VSYNC_NOTIFICATION := true
