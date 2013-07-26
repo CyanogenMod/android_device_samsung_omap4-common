12ee4e607e22db88a7844a0d5ce059605ebbc231
Andrew Jiang <sam.andrew.jiang@gmail.com>
2013-07-26 07:52:41 -0400

    omap4-common: Set TARGET_CPU_VARIANT
    Change-Id: I526918984af3e8fd9da366052a3b2fe256421505

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -28,6 +28,7 @@ TARGET_CPU_SMP := true
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_CPU_VARIANT := cortex-a9
 ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_GLOBAL_CFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
