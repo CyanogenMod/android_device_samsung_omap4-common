c2c739f44d3d11bc9b026269b4b56e1e1e2a5053
Andrew Jiang <sam.andrew.jiang@gmail.com>
2013-08-06 10:39:51 -0400

    omap4-common: Disable scissor optimization
    Reference: http://source.android.com/devices/tuning.html
    https://android.googlesource.com/device/samsung/tuna/+/fd63d70c864a1196738ab081bfd063c93081e77a
    
    Change-Id: I18634fce31e7c761d3f4bb9ac541ed4ffdb247d0

--- common.mk
+++ common.mk
@@ -55,6 +55,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.call_ring.multiple=false \
     ro.telephony.call_ring.delay=3000
 
+# SGX540 is slower with the scissor optimization enabled
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.disable_scissor_opt=true
+
 # Set default USB interface
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
