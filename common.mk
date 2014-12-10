73b77fa2d78fb0c892bb1e82dc2358101f3c9e5e
android-andi <skate4life@gmx.de>
2014-12-10 16:27:55 +0100

    omap4-common: fix Galaxy Tab 2 compileing
    Change-Id: I5165438480fbf346eac0aa18aff62d2bbd22aa44

--- common.mk
+++ common.mk
@@ -62,7 +62,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
 
+ifneq ($(filter p3100 p3110 p5100 p5110,$(TARGET_DEVICE)),)
 $(call inherit-product, hardware/ti/omap4xxx/omap4.mk)
+endif
 
 # Include non-opensource parts
 $(call inherit-product, vendor/samsung/omap4-common/common-vendor.mk)
